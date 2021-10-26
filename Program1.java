6fbac8f88c9470afed0db6c803e2320d938a8804
Aman_Prajapat <amanprajapat98801@gmail.com>
2021-10-26 19:25:17 +0530

    new logic added

--- Program1.java
+++ Program1.java
@@ -21,10 +21,19 @@ public class Program1{
         System.out.println("enter the value of y");
         y = sc.nextInt();
         
-        // logic to swap 2 numbers 
+        // logic 1 to swap 2 numbers using third variable
         int temp = x;
-        x = y;
-        y = temp;
+               x = y;
+               y = temp;
+
+    /*
+        logic 2  without using third variable
+
+        x = x+y;    //  x = 10+20 = 30
+        y = x-y;    //  y = 30-20 = 10
+        x = x-y;    //  x = 30-10 = 20
+
+    */
 
         System.out.println("after swapping values of both variables");
         System.out.println("value of x : "+x+" values of y : "+y);
