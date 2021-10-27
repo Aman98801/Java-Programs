0d9fd579dcddfc93ade42bf287d222996c8d8561
Aman_Prajapat <amanprajapat98801@gmail.com>
2021-10-27 23:08:01 +0530

    logic has been added

--- Program6.java
+++ Program6.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 /*
-  Program 6 :  count the digit in a number  
+  Program 6 :  count the number of digits in a number  
 
    input :  354546
    output :  6
@@ -8,6 +10,26 @@
 public class Program6 {
    public static void main(String[] args)
    {
+     Scanner sc = new Scanner(System.in);
      
+     System.out.println(" Enter the number");
+     int n = sc.nextInt();
+     int count=0,r;
+     if(n>0)
+     {
+
+         while(n>0)
+         {
+           r = n%10;
+           n = n/10;
+           count++;
+           System.out.print(count+" ");
+         }
+         System.out.println("total digits are : "+count);
+     }
+     else
+     {
+         System.out.println("total digits are : "+(count+1));
+     }
    }
 }
