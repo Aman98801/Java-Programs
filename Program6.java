671252e1a4c64d90966a63d07fff4b582cb2b82a
Aman_Prajapat <amanprajapat98801@gmail.com>
2021-10-27 22:59:05 +0530

    Program 6 added

---
+++ Program6.java
@@ -0,0 +1,13 @@
+/*
+  Program 6 :  count the digit in a number  
+
+   input :  354546
+   output :  6
+*/
+
+public class Program6 {
+   public static void main(String[] args)
+   {
+     
+   }
+}
