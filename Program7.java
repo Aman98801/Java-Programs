5903b406efb8843de54e7f2d68434d2f2178647e
Aman_Prajapat <amanprajapat98801@gmail.com>
2021-10-28 17:08:10 +0530

    Logic has been updated

--- Program7.java
+++ Program7.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
 
 /*
-  Program 7 : Count number of EVEN and ODD digits in  a number
+  Program 7 :
+             part 1 :   Count number of EVEN and ODD digits in  a number
 
   input : 346578
 
   output :
            Even Digits : 3  (4,6,8)
            Odd Digits : 3   (3,5,7)
+
+            Part 2 : Sum of digits
+
+             input : 234
+
+  output :
+           sum of digits : 9
+
 */
 
 public class Program7 {
@@ -19,7 +28,10 @@ public class Program7 {
         int n = sc.nextInt();
 
         int r,even=0,odd=0;
-        // build logic
+
+        int x = n;     // we are storing value of n in variable x for '"sum of digits " program also 
+
+        // build logic to count even and odd digits 
         while(n>0)
         {
           r = n%10;
@@ -32,9 +44,21 @@ public class Program7 {
           }
           n = n/10;
         }
+
         System.out.println("Even digits are : "+even);
         System.out.println("Odd digits are : "+odd);
 
-        
+        //  logic for sum of digits 
+
+        int sum = 0,r1;
+        System.out.println(x);
+        while(x>0)
+        {
+          r1 = x%10;
+          sum = sum+r1;
+          x = x/10;
+        }
+        System.out.println("Sum of Digits : "+sum);
+  
     }
 }
